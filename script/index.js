d38d2f703f6a9ccaecf018c9adb1a25c2229c417
DevRahat <dev.rahatislam@gmail.com>
2024-02-19 03:50:58 +0600

    final01

--- script/index.js
+++ script/index.js
@@ -1,13 +1,62 @@
-const seats=document.querySelector('.seat-div');
-const totalSeat=document.getElementById('total-seat');
-let totalSeatValue=+totalSeat.innerText;
-let ticketCount=document.getElementById('select-ticket');
-let ticketBuy=+ticketCount.innerText;
-const ticketPrice=document.getElementById('price');
-const tPrice=+ticketPrice.innerText;
+// const seats=document.querySelector('.seat-div');
+// const totalSeat=document.getElementById('total-seat');
+// let totalSeatValue=+totalSeat.innerText;
+// let ticketCount=document.getElementById('select-ticket');
+// let ticketBuy=+ticketCount.innerText;
+// const ticketPrice=document.getElementById('price');
+// const tPrice=+ticketPrice.innerText;
 
-seats.addEventListener('click',function(event){
-   const seatId=event.target.id;
-   setBackgroundColorById(seatId);
+// seats.addEventListener('click',function(event){
+//    const seatId=event.target.id;
+//    setBackgroundColorById(seatId);
+//    addTableElement();
    
-})
+// })
+
+
+
+const allBtn=document.getElementsByClassName('add-btn');
+
+let count=0;
+let seatCount=40;
+for(const btn of allBtn){
+   btn.addEventListener("click", function(e){
+      count=count+1;
+      setInnerText('select-ticket',count);
+      seatCount=seatCount-1;
+      setInnerText('total-seat',seatCount);
+      
+      const price=550;
+      const quality="Business";
+      const seatId=e.target.innerText;
+      addTableElement(seatId,quality,price);
+      
+      totalCost('ticketPrice',parseInt(price));
+      grandTotalCost('grandTotal',parseInt(price));
+       e.target.style.backgroundColor="yellow";
+   });
+}
+
+function setInnerText(id,value){
+   document.getElementById(id).innerText=value;
+}
+
+function totalCost(id,value){
+      const totalCost=document.getElementById(id).innerText;
+      const convertedTotalCost=parseInt(totalCost);
+      const sum =convertedTotalCost+parseInt(value);
+      setInnerText("ticketPrice",sum);
+}
+function grandTotalCost(){
+   const totalCost=document.getElementById("ticketPrice").innerText;
+   const convertedTotalCost=parseInt(totalCost);
+   let code=document.getElementById('code').value;
+   if(code=="NEW15"){
+      setInnerText("grandTotal",convertedTotalCost-100);
+   }else{
+      setInnerText("grandTotal",convertedTotalCost);
+   }
+   
+   
+
+}
--- script/utility.js
+++ script/utility.js
@@ -1,4 +1,31 @@
-function setBackgroundColorById(elementId){
-    const element = document.getElementById(elementId);
+function setBackgroundColorByClassName(elementId){
+    const element = document.getElementsByClassName(elementId);
     element.classList.add('bg-orange-400');
 }
+
+function removeBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+}
+
+
+
+function addTableElement(seat,cls,sP){
+    let table=document.getElementById('tableBody');
+    
+    let row=document.createElement('tr');
+ 
+    let r1=document.createElement('td');
+    let r2=document.createElement('td');
+    let r3=document.createElement('td');
+    
+    r1.innerText=seat;
+    r2.innerText=cls;
+    r3.innerText=sP;
+ 
+    row.appendChild(r1);
+    row.appendChild(r2);
+    row.appendChild(r3);
+ 
+    table.appendChild(row);
+ }
